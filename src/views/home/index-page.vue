7deb809ab5d22c5fcf7e6438af1d0e855ce1b95a
kaponda mulambia <44966170+kmulambia@users.noreply.github.com>
2025-06-16 20:52:30 +0200

    Refactor project structure and update dependencies. Replaced base CSS with Tailwind CSS, added Flowbite for UI components, and updated index.html with core metadata. Removed unused components and styles, streamlined routing to use a new home view structure.

---
+++ src/views/home/index-page.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <h1>Home {{ count }}</h1>
+    <button
+      @click="count++"
+      type="button"
+      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+    >
+      Increment
+    </button>
+  </div>
+</template>
